e34f5648055dfb27191ccf5b287040e26d69e284
Gin <2082660803@qq.com>
2023-01-03 18:13:53 +0800

    first commit

---
+++ Assets/Scripts/FoldPanel.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// 折叠菜单
+/// </summary>
+public class FoldPanel : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject panelItem; // 折叠页
+    [SerializeField]
+    private TitleItem titleItem;
+    [SerializeField]
+    private DataItem dataItem;
+
+    public List<FoldData> dataList = new List<FoldData>();
+
+    private void Start()
+    {
+        Create();
+    }
+
+    public void Create()
+    {
+        for (int i = 0; i < dataList.Count; i++)
+        {
+            // 创建标题
+            TitleItem title = Instantiate(titleItem).GetComponent<TitleItem>();
+            title.SetTitle(dataList[i].titleName);
+            title.transform.SetParent(this.transform);
+            title.gameObject.SetActive(true);
+
+            // 创建子折叠面板
+            GameObject panel = Instantiate(panelItem);
+            panel.transform.SetParent(this.transform);
+            // 260是折叠页的宽度，30DataItem的高度
+            //panel.GetComponent<RectTransform>().sizeDelta = new Vector3(260, 30 * dataList[i].data.Count);
+            title.SetFoldPanel(panel);
+            panel.SetActive(false);
+
+            // 创建折叠页数据
+            for (int j = 0; j < dataList[i].data.Count; j++)
+            {
+                DataItem item = Instantiate(dataItem).GetComponent<DataItem>();
+                item.transform.SetParent(panel.transform);
+                item.gameObject.SetActive(true);
+                item.SetInfo(dataList[i].data[j]);
+            }
+        }
+    }
+}
+
+
+[System.Serializable]
+public class FoldData
+{
+    public string titleName;
+    public List<ItemData> data;
+}
+
+[System.Serializable]
+public class ItemData
+{
+    public string userName;
+    //public string imageName;
+    //public Sprite imageName;
+}
